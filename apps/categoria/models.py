ace2d0c21d1c03a0b6a1badd51f07419faf49465
Yaz <74805175+yasmani0@users.noreply.github.com>
2021-07-01 13:10:57 -0500

    probando imagen

--- apps/categoria/models.py
+++ apps/categoria/models.py
@@ -1,12 +1,15 @@
 from django.db import models
-from cloudinary.models import CloudinaryField
+#from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+
 class Categoria(models.Model):
-    nombre = models.CharField(max_length=100, blank=False, unique=True, null=False)
+    nombre = models.CharField(
+        max_length=100, blank=False, unique=True, null=False)
     descripcion = models.CharField(max_length=200, null=False)
-    imagen = CloudinaryField('imagen', null=True)
+    imagen = models.ImageField(upload_to='img/categoria', null=True)
     estado = models.CharField(max_length=2, blank=True, null=True, default="1")
+
     def __str__(self):
         return '{}'.format(self.nombre)
-
